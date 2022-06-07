b022fb73ecc60ea2f4cbd5f89a869ee8053c227f
EdoardoRagazzi <edo.ragazzi@gmail.com>
2022-06-07 21:03:20 +0200

    init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import router from "./routes";
+import VueRouter from "vue-router";
+import Vue from "vue";
+
+window.Vue = require("vue").default;
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: "#app",
+    router: router,
+});
