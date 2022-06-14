25729571282cfd04f99cf02cc3d0c07a510d05b7
EdoardoRagazzi <edo.ragazzi@gmail.com>
2022-06-14 22:47:48 +0200

    components

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,25 @@
+<template>
+
+<div>
+        <bookable-list-item> </bookable-list-item>
+        <bookable-list-item> </bookable-list-item>
+        <bookable-list-item> </bookable-list-item>
+        <bookable-list-item> </bookable-list-item>
+
+</div>
+
+</template>
+
+<script>
+import  BookableListItem  from "./BookableListItem";
+
+export default {
+   
+   components: {
+    BookableListItem,
+   }
+
+};
+</script>
+
+
